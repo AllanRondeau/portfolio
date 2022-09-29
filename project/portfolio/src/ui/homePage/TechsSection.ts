25b8aa0be6d54a4b8439b095559c2ae16d07287d
AllanRondeau <sdrondeauallan@gmail.com>
2022-09-29 10:00:13 +0200

    responsive for tech section

--- project/portfolio/src/ui/homePage/TechsSection.ts
+++ project/portfolio/src/ui/homePage/TechsSection.ts
@@ -15,25 +15,31 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
           </select>
         </aside>
         <article *ngIf="selectedTechs === 'front'">
-          <div>
-            <img src="assets/SCSS/images/htmlIcon.svg" alt="htmlIcon">
-            <img src="assets/SCSS/images/cssIcon.svg" alt="cssIcon">
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/htmlIcon.svg" alt="htmlIcon">
+              <img src="assets/SCSS/images/cssIcon.svg" alt="cssIcon">
+            </div>
             <article>
               <h6>HTML & CSS</h6>
               <p>Started to learn it since <span>september 2021</span>. Those language were the first instructed in my developpement
                 degree. </p>
             </article>
           </div>
-          <div>
-            <img src="assets/SCSS/images/jsIcon.svg" alt="jsIcon">
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/jsIcon.svg" alt="jsIcon">
+            </div>
             <article>
               <h6>JavaScript</h6>
               <p>Started to learn it since <span>january 2022</span> by self-training and udemy online lesson. </p>
             </article>
           </div>
-          <div>
-            <img src="assets/SCSS/images/tsIcon.svg" alt="tsIcon">
-            <img src="assets/SCSS/images/sassIcon.svg" alt="sassIcon">
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/tsIcon.svg" alt="tsIcon">
+              <img src="assets/SCSS/images/sassIcon.svg" alt="sassIcon">
+            </div>
             <article class="last">
               <h6>TypeScript & SASS</h6>
               <p>Started to learn it since <span>june 2022</span>. I was introduce to those technologies during my first year degree
@@ -42,23 +48,29 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
           </div>
         </article>
         <article *ngIf="selectedTechs === 'back'">
-          <div>
-            <img src="assets/SCSS/images/mysqlIcon.svg" alt="sqlIcon">
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/mysqlIcon.svg" alt="sqlIcon">
+            </div>
             <article>
               <h6>My sql</h6>
               <p>Started to learn it since <span>november 2021</span>.</p>
             </article>
           </div>
-          <div>
-            <img src="assets/SCSS/images/phpIcon.svg" alt="phpIcon">
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/phpIcon.svg" alt="phpIcon">
+            </div>
             <article>
               <h6>PHP</h6>
               <p>Started to learn it since <span>december 2021</span>. It's the first back web language that we learned in class. At
                 first it was to develop my portfolio's back office.</p>
             </article>
           </div>
-          <div>
-            <img src="assets/SCSS/images/tsIcon.svg" alt="tsIcon">
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/tsIcon.svg" alt="tsIcon">
+            </div>
             <article class="last">
               <h6>TypeScript</h6>
               <p>Started to learn it during my first year degree internship on <span>june 2022</span>. I used it to create the domain
@@ -67,15 +79,19 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
           </div>
         </article>
         <article *ngIf="selectedTechs === 'framework'">
-          <div>
-            <img src="assets/SCSS/images/bootstrapIcon.svg" alt="bootstrapIcon">
-            <article >
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/bootstrapIcon.svg" alt="bootstrapIcon">
+            </div>
+            <article>
               <h6>Bootstrap</h6>
               <p>Started to learn it in <span>november 2021</span> to create a front faster on school projects.</p>
             </article>
           </div>
-          <div>
-            <img src="assets/SCSS/images/angularIcon.svg" alt="angularIcon">
+          <div class="techsDetails">
+            <div>
+              <img src="assets/SCSS/images/angularIcon.svg" alt="angularIcon">
+            </div>
             <article class="last">
               <h6>Angular</h6>
               <p>Started to learn it during my first year degree internship on <span>june 2022</span>.</p>
